bf54fe2f1301e173f3d8d907f7dbb96fde5a0b8d
Mariana Marasoiu <mariana.marasoiu@gmail.com>
2018-12-24 17:23:07 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pipelineinfrastructure",
+    version="0.0.1",
+    url="https://github.com/AfricasVoices/Pipeline-Infrastructure",
+    
+    packages=find_packages(exclude=('test',)),
+    
+    install_requires=[],
+    python_requires='>=3.6.0'
+)
