5bb1892e74db2a89aa82d55018e7aaef0c09f69f
Mariana Marasoiu <mariana.marasoiu@gmail.com>
2018-12-24 20:27:55 +0000

    Change project name in setup.py to match CoreDataModules

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pipelineinfrastructure",
+    name="PipelineInfrastructure",
     version="0.0.1",
     url="https://github.com/AfricasVoices/Pipeline-Infrastructure",
     
