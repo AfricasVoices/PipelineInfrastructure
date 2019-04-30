9004f5ca0f4d0b1e458575e79097d50a8b911fc0
Alexander Simpson <as2388@cam.ac.uk>
2019-04-30 14:13:45 +0300

    Add CoreDataModules as a project dependency

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="PipelineInfrastructure",
     version="0.0.1",
+    python_requires='>=3.6.0',
     url="https://github.com/AfricasVoices/Pipeline-Infrastructure",
-    
     packages=find_packages(exclude=("test",)),
-    
     install_requires=["firebase_admin", "google-cloud-firestore", "google-cloud-storage", "google-api-python-client",
-                      "oauth2client"],
-    python_requires='>=3.6.0'
+                      "oauth2client", "CoreDataModules"],
+    dependency_links=["git+https://git@github.com/AfricasVoices/CoreDataModules.git#egg=CoreDataModules"]
 )
