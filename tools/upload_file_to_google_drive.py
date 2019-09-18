a5309d3ac72c333e6a00c78eb3fea9b409a5225c
Alexander Simpson <as2388@cam.ac.uk>
2019-09-18 11:32:56 +0100

    Move existing tools to a new tools directory

--- tools/upload_file_to_google_drive.py
+++ tools/upload_file_to_google_drive.py
@@ -1,4 +1,4 @@
-import drive_client_wrapper as dcw
+import storage.google_drive.drive_client_wrapper as dcw
 
 import sys
 
@@ -10,7 +10,7 @@ if (len(sys.argv) != 4):
     exit(1)
 
 SERVICE_ACCOUNT_CREDENTIALS_FILE = sys.argv[1]
-dcw.init_client(SERVICE_ACCOUNT_CREDENTIALS_FILE)
+dcw.init_client_from_file(SERVICE_ACCOUNT_CREDENTIALS_FILE)
 
 FILE_TO_UPLOAD = sys.argv[2]
 DRIVE_FOLDER_PATH = sys.argv[3]
