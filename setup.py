6fde2c476238404804ab4733fd060f0cbb4acf94
Alexander Simpson <as2388@cam.ac.uk>
2020-08-25 18:46:32 +0100

    Add coredatamodules to install_requires

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     url="https://github.com/AfricasVoices/Pipeline-Infrastructure",
     packages=find_packages(exclude=("test",)),
     install_requires=["firebase_admin", "google-cloud-firestore", "google-cloud-storage", "google-api-python-client",
-                      "oauth2client", "coredatamodules"]
+                      "oauth2client", "coredatamodules @ git+https://github.com/AfricasVoices/CoreDataModules"]
 )
