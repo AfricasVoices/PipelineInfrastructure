1cf2facfab04af6e8e9c8e69514755bf9cbbd9cd
Alexander Simpson <as2388@cam.ac.uk>
2020-08-25 18:22:45 +0100

    Remove dependency links

--- setup.py
+++ setup.py
@@ -7,6 +7,5 @@ setup(
     url="https://github.com/AfricasVoices/Pipeline-Infrastructure",
     packages=find_packages(exclude=("test",)),
     install_requires=["firebase_admin", "google-cloud-firestore", "google-cloud-storage", "google-api-python-client",
-                      "oauth2client", "coredatamodules"],
-    dependency_links=["git+https://git@github.com/AfricasVoices/CoreDataModules.git#egg=coredatamodules"]
+                      "oauth2client", "coredatamodules"]
 )
