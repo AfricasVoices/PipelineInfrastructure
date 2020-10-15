7affafbca87a6b8f985892a2f6c9a24d3f30cdf4
Isaack Mwenda <mwendaisaac@gmail.com>
2020-10-15 11:05:45 +0300

    add pipeline logs table firestore wrapper

---
+++ pipeline_logs/firestore_pipeline_logs_table.py
@@ -0,0 +1,37 @@
+import firebase_admin
+from core_data_modules.logging import Logger
+from firebase_admin import credentials, firestore
+
+
+log = Logger(__name__)
+
+
+class FirestorePipelinesLogsTable(object):
+
+    def __init__(self, cert):
+        """
+        :param cert: Path to a certificate file or a dict representing the contents of a certificate.
+        :type cert: str or dict
+        """
+        cred = credentials.Certificate(cert)
+        firebase_admin.initialize_app(cred)
+        self.client = firestore.client()
+
+
+    def _get_pipeline_log_doc_ref(self, pipeline_name, timestamp_string):
+        return self.client.document(f"metrics/pipeline_logs/{pipeline_name}/{timestamp_string}")
+
+
+    def update_pipeline_logs(self, pipeline_name, timestamp_string, pipeline_logs):
+        """
+        Updates the pipeline logs for the given a pipeline run.
+
+        :param pipeline_name: Name of pipeline to update the pipeline logs of.
+        :type pipeline_name: str
+        :param timestamp_string: ISO 8601 formatted timestamp string to update the pipeline logs of.
+        :type timestamp_string: str
+        :param pipeline_logs: Pipeline logs to update with.
+        :type pipeline_logs: pipeline_monitoring.pipeline_logs.PipelineLogs
+        """
+        log.info(f"Updating Pipeline Logs for project {pipeline_name} at time {timestamp_string}...")
+        self._get_pipeline_log_doc_ref(pipeline_name, timestamp_string).set(pipeline_logs.to_dict())
