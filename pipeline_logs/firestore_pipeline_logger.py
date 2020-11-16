dbb698d55b0dc153734410066d42d45a8f37bcca
Isaack Mwenda <mwendaisaac@gmail.com>
2020-11-17 00:34:16 +0300

    refactor FirestorePipelineLogger  - improve reusability

--- pipeline_logs/firestore_pipeline_logger.py
+++ pipeline_logs/firestore_pipeline_logger.py
@@ -8,30 +8,47 @@ log = Logger(__name__)
 
 class FirestorePipelineLogger(object):
 
-    def __init__(self, cert, pipeline_name, run_id):
+    def __init__(self, cert, pipeline_name, timestamp, run_id, event):
         """
         :param cert: Path to a certificate file or a dict representing the contents of a certificate.
         :type cert: str or dict
+        :param pipeline_name: Name of pipeline to update the pipeline logs of.
+        :type pipeline_name: str
+        :param timestamp: timestamp string to update the pipeline logs of.
+        :type timestamp: str
+        :param run_id: Identifier of this pipeline run.
+        :type run_id: str
+        :param event: event log name.
+        :type event: str
+
         """
+        self.pipeline_name = pipeline_name
+        self.timestamp = timestamp
+        self.run_id = run_id
+        self.event = event
         cred = credentials.Certificate(cert)
         firebase_admin.initialize_app(cred)
         self.client = firestore.client()
 
+    def _get_pipeline_log_doc_ref(self):
+        return self.client.document(f"metrics/pipeline_logs/{self.pipeline_name}/{self.timestamp}")
 
-    def _get_pipeline_log_doc_ref(self, pipeline_name, timestamp_string):
-        return self.client.document(f"metrics/pipeline_logs/{pipeline_name}/{timestamp_string}")
-
-
-    def update_pipeline_logs(self, pipeline_name, timestamp_string, pipeline_log):
+    def _log_event(self):
         """
-        Updates the pipeline logs for the given a pipeline run.
-
-        :param pipeline_name: Name of pipeline to update the pipeline logs of.
-        :type pipeline_name: str
-        :param timestamp_string: timestamp string to update the pipeline logs of.
-        :type timestamp_string: str
-        :param pipeline_log: Pipeline log to update with.
-        :type pipeline_log: dict of timestamp, run_id and event name.
+        Returns a dict of timestamp, run_id and event
         """
-        log.info(f"Updating Pipeline Logs for project {pipeline_name} at time {timestamp_string}...")
-        self._get_pipeline_log_doc_ref(pipeline_name, timestamp_string).set(pipeline_log)
+        pipeline_log = {"timestamp": self.timestamp,
+                         "run_id": self.run_id,
+                         "event": self.event}
+
+        return pipeline_log
+
+    def update_pipeline_logs(self):
+        """
+        Updates the pipeline logs for the given pipeline run.
+        """
+
+        pipeline_log = self._log_event()
+
+        log.info(f"Updating Pipeline Logs for project {self.pipeline_name} at time {self.timestamp}...")
+        self._get_pipeline_log_doc_ref().set(pipeline_log)
