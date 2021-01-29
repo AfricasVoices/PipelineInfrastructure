da4f99c1016982d88999d987b7dd9ba2e5c73578
Isaack Mwenda <mwendaisaac@gmail.com>
2021-01-29 13:00:33 +0300

    initialise FirestorePipelineLogger with constant arguments only

--- pipeline_logs/firestore_pipeline_logger.py
+++ pipeline_logs/firestore_pipeline_logger.py
@@ -8,48 +8,35 @@ log = Logger(__name__)
 
 class FirestorePipelineLogger(object):
 
-    def __init__(self,  pipeline_name, timestamp, run_id, event, cert):
+    def __init__(self,  pipeline_name, run_id, cert):
         """
         :param pipeline_name: Name of pipeline to update the pipeline logs of.
         :type pipeline_name: str
-        :param timestamp: timestamp string to update the pipeline logs of.
-        :type timestamp: str
         :param run_id: Identifier of this pipeline run.
         :type run_id: str
-        :param event: event log name.
-        :type event: str
         :param cert: Path to a certificate file or a dict representing the contents of a certificate.
         :type cert: str or dict
 
         """
         self.pipeline_name = pipeline_name
-        self.timestamp = timestamp
         self.run_id = run_id
-        self.event = event
         cred = credentials.Certificate(cert)
         firebase_admin.initialize_app(cred)
         self.client = firestore.client()
 
     def _get_pipeline_log_doc_ref(self):
-        return self.client.document(f"metrics/pipelines/pipeline_logs/{self.timestamp}")
+        return self.client.document(f"metrics/pipelines/pipeline_logs/{self.run_id}")
 
-    def log_event(self):
-        """
-        Returns a dict of pipeline name, timestamp, run_id and event
-        """
-        pipeline_log = {"pipeline_name": self.pipeline_name,
-                        "timestamp": self.timestamp,
-                         "run_id": self.run_id,
-                         "event": self.event}
 
-        return pipeline_log
-
-    def update_pipeline_logs(self):
+    def log_event(self, event_timestamp, event_name):
         """
         Updates the pipeline logs for the given pipeline run.
         """
 
-        pipeline_log = self.log_event()
+        pipeline_log = {"pipeline_name": self.pipeline_name,
+                        "run_id": self.run_id,
+                        "timestamp": event_timestamp,
+                        "event": event_name}
 
-        log.info(f"Updating Pipeline Logs for project {self.pipeline_name} at time {self.timestamp}...")
+        log.info(f"Updating Pipeline Logs for project {event_name} at time {event_timestamp}...")
         self._get_pipeline_log_doc_ref().set(pipeline_log)
